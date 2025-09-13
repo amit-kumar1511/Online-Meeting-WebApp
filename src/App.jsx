6867f20e4a7591b11eb0690f0cc5459d994a4e08
Amit kumar <amit55421kumar@gmail.com>
2025-09-14 04:12:57 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
-import React from 'react'
-import { Routes,Route } from 'react-router-dom'
-import HomePage from './Components/HomePage/HomePage'
-import Room from './Components/Room/Room'
 
-const App = () => {
+
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./Components/HomePage/HomePage";
+import Room from "./Components/Room/Room";
+
+function App() {
   return (
-    
     <Routes>
-      <Route path="/" element={<HomePage/>}/>
-      <Route path='/room/:roomId' element={<Room/>} />
-
-     
+      <Route path="/" element={<HomePage />} />
+      <Route path="/room/:roomId" element={<Room />} />
     </Routes>
-    
-  )
+  );
 }
 
-export default App
+export default App;
